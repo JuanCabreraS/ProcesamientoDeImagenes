ae9a3e040adf7d3fb6945ee37499e97d5dee312a
JuanCabreraS <111255459+JuanCabreraS@users.noreply.github.com>
2026-03-23 11:01:48 -0600

    Add files via upload

--- AR.js
+++ AR.js
@@ -1,6 +1,8 @@
 import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js";
 
+console.log("AR.js correcto cargado");
+
 const MODEL_URL = new URL("./Futbolista.glb", import.meta.url).href;
 
 const canvas = document.getElementById("arCanvas");
